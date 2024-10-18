664e66cb0f4b33d564bbb3343a9160f43dfb5edb
Chris Gurung <chris.grg7@gmail.com>
2024-10-18 22:10:45 +0545

    chore: implemented dotenv for env vars

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Api gateway is running on http://localhost:3000');
+  await app.listen(process.env.PORT);
+  console.log(`Api gateway is running on http://localhost:${process.env.PORT}`);
 }
 bootstrap();
